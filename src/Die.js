65c6286953dc1d3d4b5c01dc5d8733b233f4c38f
Rub1Ru <ianivrubinstein@gmail.com>
2020-07-01 21:44:09 -0300

    Fix bugs

--- src/Die.js
+++ src/Die.js
@@ -3,7 +3,8 @@ import "./Die.css";
 
 class Die extends Component {
   static defaultProps = {
-    numbers: ["one", "two", "three", "four", "five", "six"]
+    numbers: ["one", "two", "three", "four", "five", "six"],
+    val: 6
   }
 
   constructor(props) {
@@ -17,9 +18,10 @@ class Die extends Component {
 
   
   render() {
-    const {numbers, locked, val, disabled} = this.props;
+    const {numbers, locked, val, disabled, rolling} = this.props;
     let img = `Die fas fa-dice-${numbers[val - 1]} fa-5x `;
-    if (locked) img += "Die-locked"
+    if (locked) img += `Die-locked `;
+    if(rolling && (locked === false)) img += `Die-rolling`;
     return (
       <i className={img} onClick={this.handleClick} disabled={disabled} />
     );
